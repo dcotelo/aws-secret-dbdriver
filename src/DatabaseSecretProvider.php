39ef84399fcf3564f0008b09923633dee2837e9e
Diego Cotelo <diego4321_30@hotmail.com>
2019-12-05 10:52:04 -0300

    init

---
+++ src/DatabaseSecretProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dcotelo\AwsSecret;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseSecretProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
